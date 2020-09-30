c475368c0db6f13934e31c48be716724fbd4b838
hotyo <moeuvozhenie@tut.by>
2020-09-30 11:09:25 +0300

    feat: add solution for typical-arrays task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+    return array && array.length ? array.sort((a, b) => a - b)[0] : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return array && array.length ? array.sort((a, b) => a - b)[array.length - 1] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return array && array.length ? array.reduce((prevV, item, index, array) => prevV + item, 0) / array.length : 0;
 }
